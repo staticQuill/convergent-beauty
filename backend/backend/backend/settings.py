b4ea7a4919c086793af4c66ca8eabd6fc4b2137f
Jenni Black <jennilblack@outlook.com>
2023-02-10 12:55:31 +0000

    change user import

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -136,9 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
