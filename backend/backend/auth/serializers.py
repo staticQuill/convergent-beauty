15775decc56ae8f6c22633f39ef9fbaa906f4525
Jenni Black <jennilblack@outlook.com>
2023-02-10 12:52:45 +0000

    change user import

--- backend/backend/auth/serializers.py
+++ backend/backend/auth/serializers.py
@@ -1,8 +1,8 @@
 from abc import ABC
 
+from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.authtoken.admin import User
 from rest_framework.validators import UniqueValidator
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
