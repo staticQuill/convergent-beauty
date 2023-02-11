4505d090631d39c553f20e4cdb53c8c73da52141
Jenni Black <jennilblack@outlook.com>
2023-02-11 13:28:49 +0000

    add init in backend

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'products',
-    'rest_framework'
+    'rest_framework',
+    'search_engine'
 ]
 
 MIDDLEWARE = [
