ea33309aeb9e7d2d34020e116814e6b0c2bfe0ef
Jenni Black <jennilblack@outlook.com>
2023-02-16 17:25:10 +0000

    fix typo

--- backend/backend/recommendations/serializers.py
+++ backend/backend/recommendations/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import UserPreference
 
 
-class UserPreferenceSerializer(serializers.HyperLinkedModelSerializer):
+class UserPreferenceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserPreference
         fields = "__all__"
