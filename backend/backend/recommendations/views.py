ac4f44c835c0251d3f102e68ff1dc96fb3f819d1
Jenni Black <jennilblack@outlook.com>
2023-02-16 17:29:20 +0000

    fix capitalization of get objects

--- backend/backend/recommendations/views.py
+++ backend/backend/recommendations/views.py
@@ -10,7 +10,7 @@ from products.models import UserProduct
 
 class UserPreferenceView(APIView):
     def get(self, request) -> Response:
-        user_preference_obj = UserPreference.objects.get(User=request.user)
+        user_preference_obj = UserPreference.objects.get(user=request.user)
 
         return Response([UserPreferenceSerializer(obj).data for obj in user_preference_obj])
 
