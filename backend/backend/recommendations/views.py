6f1325bbea70c036c73776836bb8b8b206afcb45
Jenni Black <jennilblack@outlook.com>
2023-02-16 17:37:50 +0000

    fix user product iteration

--- backend/backend/recommendations/views.py
+++ backend/backend/recommendations/views.py
@@ -18,10 +18,17 @@ class UserPreferenceView(APIView):
     def _total_sensory_values(self, current_preference: dict, total_loggings: int, personal: list, community: dict, modifier: int) -> dict:
         sensory_preferences = {}
         for sensory in personal:
-            sensory_preferences[str(sensory)] += 4 * modifier
+            try:
+                sensory_preferences[str(sensory)] += 4 * modifier
+            except KeyError:
+                sensory_preferences[str(sensory)] = 4 * modifier
         for sensory_key, value in community.items():
             weight = value / total_loggings
-            sensory_preferences[str(sensory_key)] += 2 * modifier * weight
+            try:
+                sensory_preferences[str(sensory_key)] += 2 * modifier * weight
+            except KeyError:
+                sensory_preferences[str(sensory_key)] += 2 * modifier * weight
+        return sensory_preferences
 
     def _generate_modifier(self, enjoyment: list) -> int:
         if "neutral" in enjoyment or ("disliked" in enjoyment and "liked" in enjoyment):
