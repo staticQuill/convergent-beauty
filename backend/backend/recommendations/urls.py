f66ea47a1eaa8438f943b3b108eb596de13dbfe3
Jenni Black <jennilblack@outlook.com>
2023-02-17 16:42:02 +0000

    add recommendation sorting

--- backend/backend/recommendations/urls.py
+++ backend/backend/recommendations/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('preferences/', views.UserPreferenceView.as_view(), name='recommendations')
+    path('preferences/', views.UserPreferenceView.as_view(), name='preferences')
+    path('recommendations/', views.RecommendationView.as_view(), name='recommendations')
 ]
